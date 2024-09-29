0a077482101e321843119652d2b5034fe3dba123
TranBaoDuy-hutech <160829976+TranBaoDuy-hutech@users.noreply.github.com>
2024-09-29 19:20:24 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_TRENLOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Filter = "Bitmap file|*.bmp|JPEG file|*.jpg";
+            if (oFile.ShowDialog() == DialogResult.OK)
+            {
+                // Tạo Form2 với đường dẫn ảnh
+                Form2 frm = new Form2(oFile.FileName);
+                frm.MdiParent = this;
+                frm.Show();
+            }
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+    }
+}
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_TRENLOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Filter = "Bitmap file|*.bmp|JPEG file|*.jpg";
+            if (oFile.ShowDialog() == DialogResult.OK)
+            {
+                // Tạo Form2 với đường dẫn ảnh
+                Form2 frm = new Form2(oFile.FileName);
+                frm.MdiParent = this;
+                frm.Show();
+            }
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+    }
+}
